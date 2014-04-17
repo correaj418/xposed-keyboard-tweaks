5b257db39381aadd95a8a0b827ec43c5d2fa4b4b
Nick Spacek <nicks@lashpoint.com>
2014-04-16 21:50:03 -0300

    Get working in ADT

---
+++ src/main/java/ca/spacek/gkdd/Initialize.java
@@ -0,0 +1,8 @@
+package ca.spacek.gkdd;
+
+/**
+ * Created by temp on 16/04/14.
+ */
+public class Initialize implements IXposedHookLoadPackage {
+    // Settings.Secure.getString(getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD);
+}
